d97512002e0a0b167214e48babd0a5ee55ee058f
dsmercy <dsmercy9@gmail.com>
2022-05-22 02:36:39 +0530

    51. Creating a react component to test the error responses

--- clientapp/src/app/features/about/AboutPage.tsx
+++ clientapp/src/app/features/about/AboutPage.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { Container, Typography, ButtonGroup, Button } from '@mui/material'
+import agent from '../../api/agent'
 
 export default function AboutPage() {
   return (
-    <div>AboutPage</div>
+    <Container>
+            <Typography gutterBottom variant='h2'>Errors for testing purposes</Typography>
+            <ButtonGroup fullWidth>
+                <Button variant='contained' onClick={() => agent.TestErrors.get400Error().catch(error => console.log(error))}>Test 400 Error</Button>
+                <Button variant='contained' onClick={() => agent.TestErrors.get401Error().catch(error => console.log(error))}>Test 401 Error</Button>
+                <Button variant='contained' onClick={() => agent.TestErrors.get404Error().catch(error => console.log(error))}>Test 404 Error</Button>
+                <Button variant='contained' onClick={() => agent.TestErrors.get500Error().catch(error => console.log(error))}>Test 500 Error</Button>
+                <Button variant='contained' onClick={() => agent.TestErrors.getValidationError().catch(error => console.log(error))}>Test 400 Error</Button>
+            </ButtonGroup>
+           
+        </Container>
   )
 }
