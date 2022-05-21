2780636f4911c03cc075b6194f2833ac791a50fe
dsmercy <dsmercy9@gmail.com>
2022-05-22 02:19:21 +0530

    50. Centralising the axios error

--- clientapp/src/app/features/catalog/Catalog.tsx
+++ clientapp/src/app/features/catalog/Catalog.tsx
@@ -1,20 +1,20 @@
 import { useState, useEffect } from 'react';
+import agent from '../../api/agent';
 import { Product } from '../../models/product'
 import ProductList from './ProductList';
 
 export default function Catalog() {
 
   const [products, setProducts] = useState<Product[]>([]);
+  
   useEffect(() => {
-    fetch("http://localhost:5000/api/Products")
-      .then(result => result.json())
-      .then(data => setProducts(data))
+    agent.Catalog.list().then(products => setProducts(products));
   }, [])
 
 
   return (
     <>
-      <ProductList products={products}/>
+      <ProductList products={products} />
     </>
   )
 }
