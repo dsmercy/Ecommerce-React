fc808ca09a9cefd6094e6b861b6409e983dee9d5
dsmercy <dsmercy9@gmail.com>
2022-05-29 01:11:26 +0530

    72. Styling the basket page with a table

--- clientapp/src/app/features/basket/BasketPage.tsx
+++ clientapp/src/app/features/basket/BasketPage.tsx
@@ -1,4 +1,6 @@
-import { Typography } from '@mui/material';
+import { Remove, Add, Delete } from '@mui/icons-material';
+import { LoadingButton } from '@mui/lab';
+import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react'
 import agent from '../../api/agent';
 import LoadingComponent from '../../layout/LoadingComponent';
@@ -21,7 +23,46 @@ export default function BasketPage() {
 
     return (
         <>
-            <h1>Buyer Id = {basket?.buyerId}</h1>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Product</TableCell>
+                            <TableCell align="right">Price</TableCell>
+                            <TableCell align="center">Quantity</TableCell>
+                            <TableCell align="right">Subtotal</TableCell>
+                            <TableCell align="right"></TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {basket.items.map(item => (
+                            <TableRow
+                                key={item.productId}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    <Box display='flex' alignItems='center'>
+                                        <img src={item.pictureUrl} alt={item.name} style={{ height: 50, marginRight: 20 }} />
+                                        <span>{item.name}</span>
+                                    </Box>
+                                </TableCell>
+                                <TableCell align="right">${(item.price / 100).toFixed(2)}</TableCell>
+                                <TableCell align="center">
+                                    {item.quantity}
+                                </TableCell>
+                                <TableCell align="right">${((item.price / 100) * item.quantity).toFixed(2)}</TableCell>
+                                <TableCell align="right">
+                                    <LoadingButton
+                                        color='error'
+                                    >
+                                        <Delete />
+                                    </LoadingButton>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </>
     )
 }
