cb512260c27e9cef5642e47c04071bff588a755b
dsmercy <dsmercy9@gmail.com>
2022-05-29 17:59:51 +0530

    74. App initialisation - fetching the basket on app start

--- clientapp/src/app/features/basket/BasketPage.tsx
+++ clientapp/src/app/features/basket/BasketPage.tsx
@@ -1,24 +1,11 @@
-import { Remove, Add, Delete } from '@mui/icons-material';
+import { Delete } from '@mui/icons-material';
 import { LoadingButton } from '@mui/lab';
 import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
-import React, { useEffect, useState } from 'react'
-import agent from '../../api/agent';
-import LoadingComponent from '../../layout/LoadingComponent';
-import { Basket } from '../../models/basket';
+import { useStoreContext } from '../../context/StoreContext';
 
 export default function BasketPage() {
 
-    const [loading, setLoading] = useState(false);
-    const [basket, setBasket] = useState<Basket | null>(null);
-
-    useEffect(() => {
-        agent.Basket.get()
-            .then(basket => setBasket(basket))
-            .catch(error => console.log(error))
-            .finally(() => setLoading(false));
-    },[])
-
-    if (loading) return <LoadingComponent message='Loading products...' />
+    const { basket, setBasket, removeItem } = useStoreContext();
     if (!basket) return <Typography variant='h3'>Your Basket is empty</Typography>
 
     return (
--- clientapp/src/app/layout/App.tsx
+++ clientapp/src/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './styles.css';
 import Catalog from '../features/catalog/Catalog';
 import { CssBaseline, Container, createTheme, ThemeProvider } from '@mui/material';
@@ -13,11 +13,31 @@ import { ToastContainer } from 'react-toastify';
 import ServerError from '../errors/ServerError';
 import NotFound from '../errors/NotFound';
 import BasketPage from '../features/basket/BasketPage';
+import agent from '../api/agent';
+import { useStoreContext } from '../context/StoreContext';
+import { getCookie } from '../util/util';
+import LoadingComponent from './LoadingComponent';
 
 
 function App() {
 
+  const {setBasket} = useStoreContext();
+  const [loading, setLoading] = useState(true);
   const [darkMode, setDarkMode] = useState(true);
+
+  useEffect(() => {
+    const buyerId = getCookie('buyerId');
+    if (buyerId) {
+      agent.Basket.get()
+        .then(basket => setBasket(basket))
+        .catch(error => console.log(error))
+        .finally(() => setLoading(false))
+    } else {
+      setLoading(false);
+    }
+  }, [setBasket])
+
+
   const paletteType = darkMode ? 'dark' : 'light'
   const theme = createTheme({
     palette: {
@@ -27,6 +47,8 @@ function App() {
       }
     }
   })
+
+  if (loading) return <LoadingComponent message='Initialising app...' />
 
   function handleThemeChange() {
     setDarkMode(!darkMode);
