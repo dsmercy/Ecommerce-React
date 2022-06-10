f357568cd9c49c2eb99fa064e98498f1d2bf1cd4
dsmercy <dsmercy9@gmail.com>
2022-06-10 18:00:19 +0530

    92. Using async functions in redux part 1

--- clientapp/src/app/features/basket/basketSlice.ts
+++ clientapp/src/app/features/basket/basketSlice.ts
@@ -1,14 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import agent from "../../api/agent";
 import { Basket } from "../../models/basket";
 
 
 interface BasketState {
     basket: Basket | null;
+    status: string;
 }
 
 const initialState: BasketState = {
     basket: null,
+    status: 'idle'
 }
+
+export const addBasketItemAsync = createAsyncThunk<Basket, {productId: number, quantity?: number}>(
+    'basket/addBasketItemAsync',
+    async ({productId, quantity}, thunkAPI) => {
+        try {
+            return await agent.Basket.addItem(productId, quantity);
+        } catch (error: any) {
+            console.log(error);
+        }
+    }
+)
 
 export const basketSlice = createSlice({
     name:'basket',
