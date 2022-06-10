39effd4b162b67701faa3f7669c9e7f75127d2e1
dsmercy <dsmercy9@gmail.com>
2022-06-10 20:58:06 +0530

    97. Using entity adapter selectors

--- clientapp/src/app/features/catalog/Catalog.tsx
+++ clientapp/src/app/features/catalog/Catalog.tsx
@@ -2,20 +2,20 @@ import { useState, useEffect } from 'react';
 import agent from '../../api/agent';
 import LoadingComponent from '../../layout/LoadingComponent';
 import { Product } from '../../models/product'
+import { useAppDispatch, useAppSelector } from '../../store/configureStore';
+import { fetchProductsAsync, productSelectors } from './catalogSlice';
 import ProductList from './ProductList';
 
 export default function Catalog() {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [loading, setLoading] = useState(true);
+  const products= useAppSelector(productSelectors.selectAll);
+  const {productsLoaded,status} = useAppSelector(state=>state.catalog);
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
-      agent.Catalog.list()
-          .then(products => setProducts(products))
-          .catch(error => console.log(error))
-          .finally(() => setLoading(false));
-  }, [])
+if(!productsLoaded) dispatch(fetchProductsAsync())
+  }, [productsLoaded])
 
-  if (loading) return <LoadingComponent message='Loading products...' />
+  if (status.includes('pending')) return <LoadingComponent message='Loading products...' />
 
 
   return (
