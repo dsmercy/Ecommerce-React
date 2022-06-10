613ef370b6829dcb433e56adbdd53ac9ec71dbcd
dsmercy <dsmercy9@gmail.com>
2022-06-10 14:09:46 +0530

    88. Using Redux Toolkit

--- clientapp/src/app/features/contact/ContactPage.tsx
+++ clientapp/src/app/features/contact/ContactPage.tsx
@@ -1,10 +1,10 @@
 import { Button, ButtonGroup, Typography } from '@mui/material'
-import { useDispatch, useSelector } from 'react-redux'
-import { CounterState, decrement, increment } from './counterReducer'
+import { useAppDispatch, useAppSelector } from '../../store/configureStore';
+import { decrement, increment } from './counterSlice';
 
 export default function ContactPage() {
-  const { data, title } = useSelector((state: CounterState) => state);
-  const dispatch = useDispatch();
+  const { data, title } = useAppSelector(state=>state.counter);
+  const dispatch = useAppDispatch();
   return (
     <>
       <Typography variant='h2'>
@@ -14,10 +14,10 @@ export default function ContactPage() {
         The data is: {data}
       </Typography>
       <ButtonGroup>
-                <Button onClick={() => dispatch(decrement(1))} variant='contained' color='error'>Decrement</Button>
-                <Button onClick={() => dispatch(increment(1))} variant='contained' color='primary'>Increment</Button>
-                <Button onClick={() => dispatch(increment(5))} variant='contained' color='secondary'>Increment by 5</Button>
-            </ButtonGroup>
+        <Button onClick={() => dispatch(decrement(1))} variant='contained' color='error'>Decrement</Button>
+        <Button onClick={() => dispatch(increment(1))} variant='contained' color='primary'>Increment</Button>
+        <Button onClick={() => dispatch(increment(5))} variant='contained' color='secondary'>Increment by 5</Button>
+      </ButtonGroup>
     </>
   )
 }
