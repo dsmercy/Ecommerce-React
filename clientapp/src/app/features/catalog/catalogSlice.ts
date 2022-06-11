4cdddc2c471840c329cefb69d667a9e238d94516
dsmercy <dsmercy9@gmail.com>
2022-06-12 02:04:40 +0530

    112. Adding the product params to redux state

--- clientapp/src/app/features/catalog/catalogSlice.ts
+++ clientapp/src/app/features/catalog/catalogSlice.ts
@@ -1,8 +1,16 @@
 import { createEntityAdapter, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import agent from "../../api/agent";
-import { Product } from "../../models/product";
+import { Product, ProductParams } from "../../models/product";
 import { RootState } from "../../store/configureStore";
 
+interface CatalogState {
+    productsLoaded: boolean;
+    filtersLoaded: boolean;
+    status: string;
+    brands: string[];
+    types: string[];
+    productParams: ProductParams;
+}
 
 const productsAdapter = createEntityAdapter<Product>();
 
@@ -38,17 +46,33 @@ export const fetchFilters = createAsyncThunk(
         }
     }
 )
+function initParams() {
+    return {
+        pageNumber: 1,
+        pageSize: 6,
+        orderBy: 'name'
+    }
+}
 
 export const catalogSlice = createSlice({
     name: 'catalog',
-    initialState: productsAdapter.getInitialState({
+    initialState: productsAdapter.getInitialState<CatalogState>({
         productsLoaded: false,
         filtersLoaded: false,
         status: 'idle',
         brands: [],
-        types: []
+        types: [],
+        productParams: initParams(),
     }),
-    reducers: {},
+    reducers: {
+        setProductParams: (state, action) => {
+            state.productsLoaded = false;
+            state.productParams = {...state.productParams, ...action.payload};
+        },
+        resetProductParams: (state) => {
+            state.productParams = initParams();
+        }
+    },
     extraReducers: (builder => {
         ///Get All products
         builder.addCase(fetchProductsAsync.pending, (state) => {
@@ -95,3 +119,4 @@ export const catalogSlice = createSlice({
 })
 
 export const productSelectors = productsAdapter.getSelectors((state: RootState) => state.catalog);
+export const {setProductParams, resetProductParams} = catalogSlice.actions; 
