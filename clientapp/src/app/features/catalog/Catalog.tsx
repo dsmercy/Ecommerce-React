b7a889c7b70a791907c7009682b20a7cd9b9b797
dsmercy <dsmercy9@gmail.com>
2022-06-12 01:48:14 +0530

    111. Catalog page design

--- clientapp/src/app/features/catalog/Catalog.tsx
+++ clientapp/src/app/features/catalog/Catalog.tsx
@@ -1,28 +1,79 @@
+import types from '@emotion/styled';
+import { Box, Checkbox, FormControl, FormControlLabel, FormGroup, Grid, Pagination, Paper, Radio, RadioGroup, TextField, Typography } from '@mui/material';
 import { useEffect } from 'react';
 import LoadingComponent from '../../layout/LoadingComponent';
 import { useAppDispatch, useAppSelector } from '../../store/configureStore';
 import { fetchFilters, fetchProductsAsync, productSelectors } from './catalogSlice';
 import ProductList from './ProductList';
 
+const sortOptions = [
+  { value: 'name', label: 'Alphabetical' },
+  { value: 'priceDesc', label: 'Price - High to low' },
+  { value: 'price', label: 'Price - Low to high' },
+]
+
 export default function Catalog() {
   const products = useAppSelector(productSelectors.selectAll);
-  const { productsLoaded, status,filtersLoaded } = useAppSelector(state => state.catalog);
+  const { productsLoaded, status, filtersLoaded, brands, types } = useAppSelector(state => state.catalog);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     if (!productsLoaded) dispatch(fetchProductsAsync());
-}, [productsLoaded, dispatch])
+  }, [productsLoaded, dispatch])
 
-useEffect(() => {
-  if (!filtersLoaded) dispatch(fetchFilters());
-}, [filtersLoaded, dispatch]);
+  useEffect(() => {
+    if (!filtersLoaded) dispatch(fetchFilters());
+  }, [filtersLoaded, dispatch]);
 
 
   if (status.includes('pending')) return <LoadingComponent message='Loading products...' />
 
   return (
-    <>
-      <ProductList products={products} />
-    </>
+    <Grid container columnSpacing={4}>
+      <Grid item xs={3}>
+        <Paper sx={{ mb: 2 }}>
+          <TextField label="Search products" variant='outlined' />
+        </Paper>
+        <Paper sx={{ mb: 2, p: 2 }}>
+          <FormControl component='fieldset'>
+            <RadioGroup>
+              {sortOptions.map(({ value, label }) => (
+                <FormControlLabel value={value} control={<Radio />} label={label} />
+              ))}
+            </RadioGroup>
+          </FormControl>
+        </Paper>
+        <Paper sx={{ mb: 2, p: 2 }}>
+          <FormControl component='fieldset'>
+            <FormGroup>
+              {brands.map(brand => (
+                <FormControlLabel control={<Checkbox />} label={brand} key={brand} />
+              ))}
+            </FormGroup>
+          </FormControl>
+        </Paper>
+        <Paper sx={{ mb: 2, p: 2 }}>
+          <FormControl component='fieldset'>
+            <FormGroup>
+              {types.map(type => (
+                <FormControlLabel control={<Checkbox />} label={type} key={type} />
+              ))}
+            </FormGroup>
+          </FormControl>
+        </Paper>
+      </Grid>
+      <Grid item xs={9}>
+        <ProductList products={products} />
+      </Grid>
+      <Grid item xs={3} />
+      <Grid item xs={9} sx={{ mb: 2 }}>
+        <Box display='flex' justifyContent='space-between' alignItems='center'>
+          <Typography>Display 1-6 of 20 items</Typography>
+          <Pagination
+            color='secondary' size='large' count={10} page={2}
+          />
+        </Box>
+      </Grid>
+    </Grid >
   )
 }
