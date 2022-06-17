d2fdf6095129bb32ba758b7d07c208124bf3f496
dsmercy <dsmercy9@gmail.com>
2022-06-18 01:01:42 +0530

    120. Cleaning up the loading indicators in the catalog

--- clientapp/src/app/features/catalog/Catalog.tsx
+++ clientapp/src/app/features/catalog/Catalog.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Pagination, Paper, Typography } from '@mui/material';
+import { Grid, Paper } from '@mui/material';
 import { useEffect } from 'react';
 import AppPagination from '../../components/AppPagination';
 import CheckboxButtons from '../../components/CheckboxButtons';
@@ -17,7 +17,7 @@ const sortOptions = [
 
 export default function Catalog() {
   const products = useAppSelector(productSelectors.selectAll);
-  const { productsLoaded, status, filtersLoaded, brands, types, productParams ,metaData} = useAppSelector(state => state.catalog);
+  const { productsLoaded, filtersLoaded, brands, types, productParams ,metaData} = useAppSelector(state => state.catalog);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
@@ -29,7 +29,7 @@ export default function Catalog() {
   }, [filtersLoaded, dispatch]);
 
 
-  if (status.includes('pending')||!metaData) return <LoadingComponent message='Loading products...' />
+  if (!filtersLoaded) return <LoadingComponent message='Loading products...' />
 
   return (
     <Grid container columnSpacing={4}>
