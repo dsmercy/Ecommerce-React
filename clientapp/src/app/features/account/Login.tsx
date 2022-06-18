348c0b26f8a67c595f5010d35da33d572b56fd75
dsmercy <dsmercy9@gmail.com>
2022-06-18 21:32:30 +0530

    137. Adding an account slice

--- clientapp/src/app/features/account/Login.tsx
+++ clientapp/src/app/features/account/Login.tsx
@@ -10,20 +10,26 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { Paper } from '@mui/material';
 import agent from '../../api/agent';
 import { FieldValues, useForm } from 'react-hook-form';
 import { LoadingButton } from '@mui/lab';
+import { useAppDispatch } from '../../store/configureStore';
+import { signInUser } from './accountSlice';
 
 
 export default function Login() {
+    const history = useHistory();
+    const dispatch = useAppDispatch();
+
     const { register, handleSubmit, formState: { isSubmitting, errors, isValid } } = useForm({
         mode: 'all'
     });
 
     async function submitForm(data:FieldValues) {
-        await agent.Account.login(data);
+        await dispatch(signInUser(data));
+            history.push('/catalog');
     }
 
     return (
