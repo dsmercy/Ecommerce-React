a610b8489fc1065a60b452f918c715f2ba05122a
dsmercy <dsmercy9@gmail.com>
2022-06-18 20:46:12 +0530

    134. Using controlled component

--- clientapp/src/app/features/account/Login.tsx
+++ clientapp/src/app/features/account/Login.tsx
@@ -12,64 +12,74 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { Link } from 'react-router-dom';
 import { Paper } from '@mui/material';
+import agent from '../../api/agent';
 
 
 export default function Login() {
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+
+    const [values, setValues] = React.useState(
+        {
+            username: '',
+            password: ''
+        }
+    );
+
+
+    const handleSubmit = (event:any) => {
         event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get('email'),
-            password: data.get('password'),
-        });
+       agent.Account.login(values);
     };
+    function handleInputChange(event: any) {
+        const { name, value } = event.target;
+        setValues({ ...values, [name]: value });
+    }
 
     return (
         <Container component={Paper} maxWidth="sm" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', p: 4 }}>
             <CssBaseline />
-                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Sign in
-                </Typography>
-                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                    />
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{ mt: 3, mb: 2 }}
-                    >
-                        Sign In
-                    </Button>
-                    <Grid container>
-                        <Grid item>
-                            <Link to='/register'>
-                                {"Don't have an account? Sign Up"}
-                            </Link>
-                        </Grid>
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+                Sign in
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    label="Username"
+                    name="username"
+                    autoFocus
+                    onChange={handleInputChange}
+                    value={values.username}
+                />
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    onChange={handleInputChange}
+                    value={values.password}
+                />
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                >
+                    Sign In
+                </Button>
+                <Grid container>
+                    <Grid item>
+                        <Link to='/register'>
+                            {"Don't have an account? Sign Up"}
+                        </Link>
                     </Grid>
-                </Box>
+                </Grid>
+            </Box>
         </Container>
     );
 }
