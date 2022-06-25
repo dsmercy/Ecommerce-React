6d04ac72d89efaaba0fb7bab77d98025142b88a7
dsmercy <dsmercy9@gmail.com>
2022-06-26 00:51:33 +0530

    157. Using react hook form context

--- clientapp/src/app/features/checkout/AddressForm.tsx
+++ clientapp/src/app/features/checkout/AddressForm.tsx
@@ -2,18 +2,16 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import { useForm } from 'react-hook-form';
+import { useFormContext } from 'react-hook-form';
 import AppTextInput from '../../components/AppTextInput';
-import { Button } from '@mui/material';
 
 export default function AddressForm() {
-    const { control, handleSubmit } = useForm();
+    const { control } = useFormContext();
     return (
         <>
             <Typography variant="h6" gutterBottom>
                 Shipping address
             </Typography>
-            <form onSubmit={handleSubmit((data) => console.log(data))}>
                 <Grid container spacing={3}>
                     <Grid item xs={12} sm={12}>
                         <AppTextInput control={control} name='fullName' label='Full name' />
@@ -43,8 +41,6 @@ export default function AddressForm() {
                         />
                     </Grid>
                 </Grid>
-                <Button type='submit'>Submit Form</Button>
-            </form>
         </>
     );
 }
